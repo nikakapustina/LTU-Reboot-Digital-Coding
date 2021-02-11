b252c98d391f5b525d8004e558b01a072d7cc790
nikakapustina <77464345+nikakapustina@users.noreply.github.com>
2021-02-11 20:11:32 +0000

    Update task-3.1.2.js
    Add single and multiple line comments
    Add alert
    console.log
    document.write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,12 @@
+/*
+this is a
+multiple line
+comment
+*/
+
+
+//this is a single line comment
+
+alert("Hello from alert box"); //this pops up with an alert message
+console.log("Hello from the console"); //shows a message in the console
+document.write("Hello from the document.wright"); // prints a message to the screen 
