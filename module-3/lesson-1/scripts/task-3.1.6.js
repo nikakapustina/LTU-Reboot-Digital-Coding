7cc6b9588f7486fae5dc62fad107d791008f8260
nikakapustina <77464345+nikakapustina@users.noreply.github.com>
2021-02-16 19:08:28 +0000

    Update task-3.1.6.js

---
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -0,0 +1,18 @@
+var mealPrice = ("£32.23");
+
+var tip = (mealPrice * 0.10);
+
+var totalBill = (mealPrice + tip);
+
+console.log (totalBill); 
+
+
+
+
+let mealPrice = 40.00;
+
+let tip =12.5;
+
+let tipAmount = mealPrice *tip) /100;
+
+console.log ("£" + (mealPrice + tipAmount).toFixed (2));
