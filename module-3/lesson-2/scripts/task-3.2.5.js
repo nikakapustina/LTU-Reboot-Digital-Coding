d24acc8fc4f49cf065edcf195c3992cc42dae011
nikakapustina <77464345+nikakapustina@users.noreply.github.com>
2021-02-16 20:31:16 +0000

    Update task-3.2.5.js

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,18 @@
+var temperature = 45;
+
+if ( temperature < 0 ) {
+
+console.log( "Stay inside" );
+
+} else if ( temperature < 30 ) {
+
+    console.log( "Wear a coat and a hat" );
+
+} else if ( temperature <50 ) {
+
+  console.log( "Wear a coat" )
+
+} else {
+
+    console.log ( "Just wear a pants")
+}
