3b7279a83e15d5801cd8d9009910565efdd793e4
nikakapustina <77464345+nikakapustina@users.noreply.github.com>
2021-02-16 20:43:03 +0000

    Update task-3.2.6.js

---
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -0,0 +1,20 @@
+var temperature = 55;
+var season = "summer";
+
+if ( temperature < 15 ) {
+
+console.log( "Wear a coat" );
+
+} else if ( temperature < 30 ) {
+
+    console.log( "Wear a jacket" );
+
+} else if ( temperature <50 ) {
+
+  console.log( "Wear a tshirt" )
+
+} else if (temperature > 50 && season === "summer" ) {
+    console.log( "Its hot outside, get yourself a beer" );
+
+}
+
