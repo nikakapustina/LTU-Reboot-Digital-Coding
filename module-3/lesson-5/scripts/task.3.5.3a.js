3b2aafbf9e80b6efeb218da91fcba851d55b3de7
nikakapustina <77464345+nikakapustina@users.noreply.github.com>
2021-02-25 20:37:54 +0000

    Create task.3.5.3a.js

---
+++ module-3/lesson-5/scripts/task.3.5.3a.js
@@ -0,0 +1,34 @@
+function discountAmount( arr, discountAmount, type ) {
+    
+    let totalPrice = 0;
+
+    for ( let index = 0; index < arr.length; index++ ) {
+
+        if( arr[ index ].type === type ) {
+
+            let discount = ( arr[ index ]. price * discountAmount ) / 100;
+
+                totalPrice = totalPrice + (arr[ index ].price - discount ) *
+                arr[ index ].quantity;
+
+        } else { if ( arr[ index ].type === "any" ) {
+
+            let discount = ( arr[ index ]. price * discountAmount ) / 100;
+
+            totalPrice = totalPrice + (arr[ index ].price - discount ) *
+                arr[ index ].quantity;
+
+        } else {
+
+            totalPrice = totalPrice + (arr[ index ].price * arr[ index ].quantity );
+                
+        }
+
+    }
+
+    return totalPrice.toFixed( 2 );
+
+    console.log( discountAmount( shoppingCart, 15, "food" ) );
+    console.log( discountAmount( shoppingCart, 50, "alcohol" ) );
+
+        
