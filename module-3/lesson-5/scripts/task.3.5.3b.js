99afe07894fadda84b0a01afe70cf80462f88d73
nikakapustina <77464345+nikakapustina@users.noreply.github.com>
2021-02-25 20:57:01 +0000

    Create task.3.5.3b.js

---
+++ module-3/lesson-5/scripts/task.3.5.3b.js
@@ -0,0 +1,17 @@
+function betweenPrices( arr, lowPrice, highPrice ) {
+
+    let arrItems = [];
+
+    for( let index = 0; index < arr.lenght; index++ ) {
+       
+        if( arr[ index ].price >= lowPrice && arr[ index ].price < highPrice ) {
+            
+            arrItems.push( arr[ index ].name );
+        }
+    }
+
+    return arrItems;
+}
+
+console.log( betweenPrices( shoppingCart, 2, 5,) );
+console.log( betweenPrices( shoppingCart, 5, 10,) );
