56600518ab7e1a7e651b4978ce1b23705fd41eff
nikakapustina <77464345+nikakapustina@users.noreply.github.com>
2021-02-25 19:43:49 +0000

    Create task-3.4.5.js

---
+++ module-3/lesson-4/scripts/task-3.4.5.js
@@ -0,0 +1,20 @@
+const favouriteRecipe = {
+    recipeTitle : "Spaghetti Bolognese",
+    servings: 6,
+    ingredients: [
+        "1 tbsp olive oil",
+        "500g mince beef",
+        "tomato sauce"
+    ],
+directions: [
+    "Make Bolognese sauce",
+    "step 2",
+    "step 3"
+    ]
+},
+letsCook: function() {
+    console.log( "I am hungry!, let's cook" + favouriteRecipe.recipeTitle );
+    },
+
+
+    favouriteRecipe.letsCook();
